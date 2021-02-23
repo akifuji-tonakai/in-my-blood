d1293a2eb03c56bf160c860ae5bb99a90792868d
akifuji-tonakai <akifujitonakai@icloud.com>
2021-02-23 17:10:00 +0900

    createアクション
    投稿２つだと詰まってしまったため、１つにした

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,8 +1,35 @@
 class UrlsController < ApplicationController
+  def new
+    @user = User.find(params[:user_id])
+    @tag = Tag.find(params[:tag_id])
+    @url = Url.new
+  end
+  def create
+    @user = User.find(params[:user_id])
+    @tag = Tag.find(params[:tag_id])
+    @url = Url.new(url_params)
+    if @url.save
+      redirect_to action: :new
+      delete_nill_url
+    else
+      render :new
+    end
+    
+  end
   def destroy
-    @url = Url.find(params[:id])
-    @tag = @url.tag
+    @url = Url.find(params[:tag_id])
+    @tag = Tag.find(params[:id])
     @url.destroy
     redirect_to controller: :tags, action: :show, id: @tag.id
   end
+
+  private
+  def url_params
+    params.require(:url).permit(:youtubepath).merge(tag_id: params[:tag_id])
+  end
+  def delete_nill_url
+    @falses = Url.where(youtubepath: "false")
+    @falses.destroy_all
+  end
+ 
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,20 @@
 class Url < ApplicationRecord
   belongs_to :tag
+
+  validates :youtubepath, presence: true, if: :change
+  
+  def change
+    if youtubepath.slice(0..12) == "https://youtu" || youtubepath.slice(0..22) == "https://www.youtube.com"
+      if youtubepath.include?("watch?v=")
+        youtubepath.gsub!("watch?v=", "embed/")
+      elsif youtubepath.include?("youtu.be")
+        youtubepath.gsub!("youtu.be", "youtube.com/embed")
+      elsif youtubepath.include?("playlist?")
+        youtubepath.gsub!("playlist","embed/videoseries")
+      end
+     else
+       youtubepath.gsub!(youtubepath,"false")
+     end
+  end
+  
 end
